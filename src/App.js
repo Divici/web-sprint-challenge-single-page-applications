c856aa2c095e6ea850518fc60855689a80f47397
david-aihe <doa9200@gmail.com>
2021-12-16 23:49:49 -0500

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import { Link, Route, Switch } from 'react-router-dom';
 import PizzaForm from './components/PizzaForm';
 import Confirmation from './components/Confirmation';
 
 const App = () => {
+
+  const initialFormValues = {
+    name: '',
+    size: '',
+    pepperoni: false,
+    sausage: false,
+    onions: false,
+    bacon: false,
+    special: '',
+  }
+
   return (
     <>
 
     <div className="container header">
-      <h1>Lambda Eats</h1>
+      <h1>Bloom Pizza</h1>
       <div className="nav">
         <Link to="/">Home</Link>
         <Link to="/pizza">Create Pizza</Link>
         <Link to="/confirmation">Past Orders</Link>
       </div>
     </div>
+    <div>
+      <img
+        className='home-image'
+        src='https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb
+        -1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=c
+        rop&w=1740&q=80'
+        alt=''
+      />
+    </div>
+      
+    
 
     <Switch>
       <Route path="/pizza">
